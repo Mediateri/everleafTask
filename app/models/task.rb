a20845d8e47ed271e35b1a4e258d516ad455b4be
mediateri <ntawukuriryayomeddy1@gmail.com>
2019-09-23 07:12:09 +0000

    step24-3

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,5 +3,5 @@ class Task < ApplicationRecord
     belongs_to :user
     validates :tittle, presence: true, uniqueness: true
     enum status: [:low, :medium, :high ]
-    paginates_per 1
+    paginates_per 4
 end
