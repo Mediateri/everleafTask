adb65b22ceb220f37eb2e99096501e6bedee98bc
mediateri <ntawukuriryayomeddy1@gmail.com>
2019-09-30 06:43:42 +0000

    hy

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_19_081305) do
 ActiveRecord::Schema.define(version: 2019_09_20_063019) do
 
   # These are extensions that must be enabled in order to support this database
