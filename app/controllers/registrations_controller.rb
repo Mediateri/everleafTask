d61be4d2a590a38267a4cb5233473c98768ea8d6
mediateri <ntawukuriryayomeddy1@gmail.com>
2019-09-30 10:20:07 +0000

    japanscode

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,4 +6,12 @@ class RegistrationsController < Devise::RegistrationsController
     def account_update_params
         params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password, :admin)
     end
+    def destroy
+        @user = User.find(params[:id])
+        if @user.tasks?
+            Task.where(user_id: params[:id]).destroy
+        end
+        @user.destroy
+        redirect_to tasks_url, notice: 'Task was successfully destroyed.'
+    end
  end
